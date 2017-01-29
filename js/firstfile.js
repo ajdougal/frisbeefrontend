26ef6b77a717e1b8cfb5f2e78fa7fc2eb8996580
Alex Dougal <adougal@adougal-mac2.wsm.local>
2017-01-29 12:06:37 -0600

    Made an html file with a button that will hopefully make an api call to php file

--- js/firstfile.js
+++ js/firstfile.js
@@ -2,3 +2,14 @@
  * Created by adougal on 1/29/17.
  */
 
+
+function makeCall(){
+    $.ajax({
+        url: "http://utmayhem.com/php/play.php",
+        type: "POST",
+        data: {testKey: "testingKey1"},
+        success: function(response){
+            console.log(response);
+        }
+    });
+}
